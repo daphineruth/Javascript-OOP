988f2e4462e05b81bb9801cba347ef8356fe9208
sheiswiz <daphneruth@304gmail.com>
2022-03-20 01:11:20 +0300

    prototypes

---
+++ OOP/script.js
@@ -0,0 +1,36 @@
+'use strict';
+const Person = function (firstName, birthYear) {
+    
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  
+ };
+  
+  const jonas = new Person('Jonas', 1991);
+  console.log(jonas);
+
+  const matilda = new Person('Matilda', 2017);
+  const jack = new Person('Jack', 1975);
+
+
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();  
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+  
+  
