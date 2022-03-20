db7b4434c3b57b37ade00d2c10aa40dd6cfef68c
sheiswiz <daphneruth@304gmail.com>
2022-03-20 09:06:43 +0300

    inheritance:constructor functions

--- OOP/script.js
+++ OOP/script.js
@@ -136,4 +136,31 @@ PersonCl.prototype.greet = function ()
   sarah.calcAge();
 
   
+
+  // Inheritance Between "Classes"  -:Constructor Functions
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName; 
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+// Linking prototypes
+Student.prototype = Object.create(Person.prototype);
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const mike = new Student('Mike', 2020, 'Computer Science');
+mike.introduce();
+mike.calcAge();
   
